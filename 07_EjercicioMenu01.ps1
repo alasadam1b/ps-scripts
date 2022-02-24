2502def002876e49901b3ebe40608e2df9d7683a
alasadam1b <alasadam1b@gmail.com>
2022-02-24 19:57:43 +0100

    Fallos Hashtable

--- 07_EjercicioMenu01.ps1
+++ 07_EjercicioMenu01.ps1
@@ -2,23 +2,21 @@ function addAlumno {
     Clear-Host
     [string]$entrada = Read-Host "Escribe el nombre"
     if (!$alumnos.ContainsValue($entrada)) {
-        $alumnos.Add($alumnos.Count + 1, $entrada)
+        
     }
     else { Write-Host "El nombre ya existe" }
 }
 function borrarAlumno {
     mostrar
-    [string]$entrada = Read-Host "Escribe el nombre a eliminar"
-    $alumnos.Add($alumnos.Count + 1, $entrada)
+    [int]$entrada = Read-Host "Escribe ... a eliminar"
+    $alumnos.Remove($entrada)
 }
 function buscarNombre {
     
 }
 function mostrar {
     Clear-Host
-    for ($i = 1; $i -le $alumnos.Count; $i++) {
-        Write-Host $alumnos[$i]
-    }
+    
 }
 
 $alumnos = @{1="juan"; 2="Oscar"; 3="Adrian"; 4="Javier"}
