ae959764ca1c30746801e74ae778387c4181e01d
alasadam1b <alasadam1b@gmail.com>
2022-03-03 20:53:52 +0100

    Nuevos ej.

---
+++ 09_EjercicioMenu03.ps1
@@ -0,0 +1,42 @@
+function mosCmdlet {
+    $alias = Read-Host "Escribe un alias"
+}
+function mosAlias {
+    
+}
+function mosAyuda {
+    
+}
+function verbosText {
+    
+}
+function cmdletText {
+    
+}
+
+function menu {
+    do {
+        Clear-Host
+        Write-Host "
+        MENÚ 03
+        1. Pedir un alias y mostrar su cmdlet
+        2. Pedir un cmdlet y motrar todos sus alias
+        3. Pedir un cmdlet y mostar su ayuda
+        4. Pedir un texto y mostrar todos los verbos que empiecen por el texto
+        5. Pedir un texto y mostrar lodos los cmdlet que empiecen por el texto
+        S. Salir
+        "
+        $op = Read-Host "Elige opción"
+        
+        switch ($op) {
+            1 { mosCmdlet }
+            2 { mosAlias }
+            3 { mosAyuda }
+            4 { verbosText }
+            5 { cmdletText }
+        }
+        Pause
+    } until ($op.ToUpper() -eq "S")
+}
+
+menu
---
+++ 10_EjercicioMenu04.ps1
@@ -0,0 +1,57 @@
+function testConn {
+    
+}
+function mostrarDat {
+    
+}
+function lsDir {
+    
+}
+function treeDirS {
+    
+}
+function treeDirC {
+    
+}
+function usuAct {
+    
+}
+function usuDes {
+    
+}
+function nomGrup {
+    
+}
+
+function menu {
+    do {
+        Clear-Host
+        Write-Host "
+        MENÚ 04
+        1. Lee un fichero ips.txt (tendras varias ips, una por línea), haz un Test-Connection y muestra un mensaje (Conexión establecida/ Conexión fallida).
+        2. Lee un fichero users.csv y muestra solo los datos de las columnas Id, Username y Location.
+        3. Pide la ruta de un directorio y lista el contenido de dicho directorio usando un cmdlet.
+        4. Pide la ruta de un directorio y muestra su estructura sin ficheros (la función será la misma en el punto 4 y 5).
+        5. Pide la ruta de un directorio y muestra su estructura con ficheros (la función será la misma en el punto 4 y 5).
+        6. Lista los usuarios locales que estén activados (la función será la misma en el punto 6 y 7).
+        7. Lista los usuarios locales que estén desactivados (la función será la misma en el punto 6 y 7).
+        8. Lista los grupos locales y mandalos solo su nombre a un fichero grupos.txt en orden descendente. 
+        S. Salir
+        "
+        $op = Read-Host "Elige opción"
+        
+        switch ($op) {
+            1 { testConn }
+            2 { mostrarDat }
+            3 { lsDir }
+            4 { treeDirS }
+            5 { treeDirC }
+            6 { usuAct }
+            7 { usuDes }
+            8 { nomGrup }
+        }
+        Pause
+    } until ($op.ToUpper() -eq "S")
+}
+
+menu
