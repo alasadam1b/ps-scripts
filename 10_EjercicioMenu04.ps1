3463c407f68f1b827e2f4c5d5112c33693fb7b6a
alasadam1b <alasadam1b@gmail.com>
2022-03-07 20:57:54 +0100

    Ej.10

--- 10_EjercicioMenu04.ps1
+++ 10_EjercicioMenu04.ps1
@@ -1,8 +1,22 @@
 function testConn {
-    
+    Clear-Host
+    foreach($row in Get-Content .\ips.txt) {
+        if (Test-Connection $row) {
+            Write-Host $row" - Conexión establecida"
+        }
+        else {
+            Write-Host $row" - Conexión fallida"
+        }
+    }
 }
 function mostrarDat {
-    
+    Clear-Host
+    $listOfRows = Import-Csv ".\users.csv" -Delimiter ";"
+    $salida=""
+    foreach($row in $listOfRows) {
+        $salida = Write-Host $row.Id $row.Username $row.Location
+    }
+    $salida
 }
 function lsDir {
     $dir = Read-Host "Escribe la ruta a listar"
@@ -30,14 +44,22 @@ function menu {
         Clear-Host
         Write-Host "
         MENÚ 04
-        1. Lee un fichero ips.txt (tendras varias ips, una por línea), haz un Test-Connection y muestra un mensaje (Conexión establecida/ Conexión fallida).
-        2. Lee un fichero users.csv y muestra solo los datos de las columnas Id, Username y Location.
-        3. Pide la ruta de un directorio y lista el contenido de dicho directorio usando un cmdlet.
-        4. Pide la ruta de un directorio y muestra su estructura sin ficheros (la función será la misma en el punto 4 y 5).
-        5. Pide la ruta de un directorio y muestra su estructura con ficheros (la función será la misma en el punto 4 y 5).
-        6. Lista los usuarios locales que estén activados (la función será la misma en el punto 6 y 7).
-        7. Lista los usuarios locales que estén desactivados (la función será la misma en el punto 6 y 7).
-        8. Lista los grupos locales y mandalos solo su nombre a un fichero grupos.txt en orden descendente. 
+        1. Lee un fichero ips.txt (tendras varias ips, una por línea), haz
+           un Test-Connection y muestra un mensaje (Conexión establecida/ Conexión fallida).
+        2. Lee un fichero users.csv y muestra solo los datos de las columnas
+           Id, Username y Location.
+        3. Pide la ruta de un directorio y lista el contenido de dicho
+           directorio usando un cmdlet.
+        4. Pide la ruta de un directorio y muestra su estructura sin
+           ficheros (la función será la misma en el punto 4 y 5).
+        5. Pide la ruta de un directorio y muestra su estructura con
+           ficheros (la función será la misma en el punto 4 y 5).
+        6. Lista los usuarios locales que estén activados (la función
+           será la misma en el punto 6 y 7).
+        7. Lista los usuarios locales que estén desactivados (la función
+           será la misma en el punto 6 y 7).
+        8. Lista los grupos locales y mandalos solo su nombre a un fichero
+           grupos.txt en orden descendente. 
         S. Salir
         "
         $op = Read-Host "Elige opción"
